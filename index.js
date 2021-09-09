c1e6e3f6b1d7ca22714e96602506fee5f729e33c
Kimmo Suominen <kitasuo@gmail.com>
2021-09-09 15:49:16 +0300

    thing

--- index.js
+++ index.js
@@ -5,14 +5,52 @@ var cors = require("cors");
 
 app.use(cors());
 
+let lastIndex = 1;
+
 let database = [
-  { id: 1, latitude: 60, longitude: 70 },
-  { id: 2, latitude: 40, longitude: 80 },
+  { id: lastIndex++, latitude: 60, longitude: 70 },
+  { id: lastIndex++, latitude: 40, longitude: 80 },
 ];
+
+app.use(express.json());
 
 app.use(express.static("public"));
 app.get("/api/locations", function (req, res) {
   res.json(database);
+});
+
+// GET http://localhost:8080/customers/99
+app.get("/api/locations/:myId([0-9]+)", (req, res) => {
+  let id = req.params.myId;
+
+  // etsi taulukosta henkilö joka id mätsää annetun kanssa
+  let location = database.find((location) => location.id == id);
+
+  if (location != null) {
+    res.send(location);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// DELETE http://localhost:8080/customers/99
+app.delete("/api/locations/:myId([0-9]+)", (req, res) => {
+  let id = req.params.myId;
+  let newDB = database.filter((location) => location.id != id);
+  if (newDB.length != database.length) {
+    database = newDB;
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+// POST http://localhost:8080/customers/
+app.post("/api/locations/", (req, res) => {
+  let body = req.body;
+  body.id = lastIndex++;
+  database.push(body);
+  res.status(201).send(body);
 });
 
 app.listen(port, () => {
