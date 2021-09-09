738f94309c405e5967d46223945713818c46c935
Kimmo Suominen <kitasuo@gmail.com>
2021-09-09 13:29:43 +0300

    Lab 5

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// serve the public directory as a static content here
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
 app.use(express.static("public"));
+app.get("/api/locations", function (req, res) {
+  res.json(database);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
